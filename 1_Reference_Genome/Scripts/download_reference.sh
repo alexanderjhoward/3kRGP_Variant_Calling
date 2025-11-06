9db3ed3a722030ac53f1379f86d94f8c55be3492
Alexander Howard <67879164+alexanderjhoward@users.noreply.github.com>
2025-11-06 13:33:47 -0800

    Update download_reference.sh

--- 1_Reference_Genome/Scripts/download_reference.sh
+++ 1_Reference_Genome/Scripts/download_reference.sh
@@ -18,7 +18,7 @@ set -x
 # Download and index reference genome
 wget -P Source/ https://rapdb.dna.affrc.go.jp/download/archive/irgsp1/IRGSP-1.0_genome.fasta.gz
 gunzip Source/IRGSP-1.0_genome.fasta.gz
-samtools faidx Source/IRGSP-1.0_genome.fasta.gz
+samtools faidx Source/IRGSP-1.0_genome.fasta
 
 # Download reference genome annotations
 wget -P Source/ https://rapdb.dna.affrc.go.jp/download/archive/irgsp1/IRGSP-1.0_representative_2024-07-12.tar.gz
