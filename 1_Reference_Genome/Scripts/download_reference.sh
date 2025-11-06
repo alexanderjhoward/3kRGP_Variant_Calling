3637a8c25fd4141a07d4372c9b0c814efcdcdf65
Alexander Howard <67879164+alexanderjhoward@users.noreply.github.com>
2025-11-06 13:40:08 -0800

    Update download_reference.sh

--- 1_Reference_Genome/Scripts/download_reference.sh
+++ 1_Reference_Genome/Scripts/download_reference.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=adamgrp             # account
 #SBATCH -p low2                       # partition
 #SBATCH -c 1                          # 1 cores
-#SBATCH -t 0:30:00                    # 30 minute reservation
-#SBATCH --mem=32000                   # 32 Gb memory
+#SBATCH -t 0:15:00                    # 15 minute reservation
+#SBATCH --mem=8000                    # 8 Gb memory
 
 # Initialize conda
 . ~/mambaforge/etc/profile.d/conda.sh
@@ -22,7 +22,7 @@ samtools faidx Source/IRGSP-1.0_genome.fasta
 
 # Download reference genome annotations
 wget -P Source/ https://rapdb.dna.affrc.go.jp/download/archive/irgsp1/IRGSP-1.0_representative_2024-07-12.tar.gz
-tar -xvzf Source/IRGSP-1.0_representative_2024-07-12.tar.gz
+tar -xvzf Source/IRGSP-1.0_representative_2024-07-12.tar.gz -C Source/
 rm Source/IRGSP-1.0_representative_2024-07-12.tar.gz
 
 # Print out final statistics about resource use before job exits
